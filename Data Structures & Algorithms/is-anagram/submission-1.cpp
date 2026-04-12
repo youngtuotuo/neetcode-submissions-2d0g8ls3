e7db426575c033d560cafc79b1fe3c678b3aaad2
Tuo Hung <56819408+youngtuotuo@users.noreply.github.com>
2026-04-12 13:12:02 +0800

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int hash[26] = {0};
+        int i = 0;
+        while (s[i]) hash[s[i++] - 'a']++;
+        i = 0;
+        while (t[i]) hash[t[i++] - 'a']--;
+        for (i = 0; i < 26; i++) {
+            if (hash[i] != 0) return false;
+        }
+        return true;
+    }
+};
